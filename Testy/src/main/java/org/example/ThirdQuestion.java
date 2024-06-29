caf8246cffb1427223d6b869631a80ba4fb1032a
B3ze <128479519+B3ze@users.noreply.github.com>
2024-06-29 21:13:04 +0300

    Update ThirdQuestion.java

--- Testy/src/main/java/org/example/ThirdQuestion.java
+++ Testy/src/main/java/org/example/ThirdQuestion.java
@@ -6,10 +6,10 @@ public class ThirdQuestion {
     public static void main(String[] args) {
 
         int [] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for(int b = 0; b < 10; b++)
-        { if (a[b] % 2 == 0){
+        for(int b = 0; b < 10; b++) {
+            if (a[b] % 2 == 0) {
             System.out.println(a[b]);
-        } else continue;
+            };
         }
     }
 }
